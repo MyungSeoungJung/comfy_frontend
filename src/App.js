8a0cb129f35387b12712eb2b979fb9ce6b625329
yoon <whyjmlearn@gmail.com>
2024-03-02 18:23:01 +0900

    3월 2일 메인페이지 구현

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./reset.css";
 import Login from "./component/Login.js";
 import Signup from "./component/Signup.js";
+import Main from "./component/Main.js";
+import ChatWidget from "./component/ChatWidget.js";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+    <div className="App" id="App">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      {/* 채팅위젯 */}
+      <ChatWidget />
+    </div>
   );
 }
 
