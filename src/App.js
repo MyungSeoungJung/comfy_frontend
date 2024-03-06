46b1fd6e9d849864a3e0b8ef637670611cff49eb
yoon <whyjmlearn@gmail.com>
2024-03-06 18:08:46 +0900

    3월 6일 카카오지도API 대기 및 위젯생성완료

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
 import React, { useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom"; import "./reset.css";
-import { getCookie } from './utils/cookie.js';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "./reset.css";
+import { getCookie } from "./utils/cookie.js";
 import Login from "./component/Login.js";
 import Signup from "./component/Signup.js";
 import LayOut from "./component/LayOut.js";
-import ChatWidget from "./component/ChatWidget.js";
-import CreateStudyGruop from "./component/study/CreateStudyGroup.js"
+import ChatWidget from "./component/chatbot/ChatWidget.js";
+// import CreateStudyGruop from "./component/study/CreateStudyGroup.js";
 import Home from "./component/Home.js";
+import RecoWidget from "./component/reco/RecoWidget.js";
 function App() {
+  // const navigate = useNavigate();
 
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = getCookie('token');
-    if (!token) {
-      navigate('/login', { replace: true });
-    }
-  }, [navigate]);
+  // useEffect(() => {
+  //   const token = getCookie("token");
+  //   if (!token) {
+  //     navigate("/login", { replace: true });
+  //   }
+  // }, [navigate]);
 
   return (
     <div className="App" id="App">
       <Routes>
         <Route path="/Login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/SignUp" element={<Signup />} />c
         <Route path="/" element={<LayOut />}>
           <Route index element={<Home />} />
-          <Route key="CreateStudyGruop" path="CreateStudyGruop" element={<CreateStudyGruop />} />
+          {/* <Route key="CreateStudyGruop" path="CreateStudyGruop" element={<CreateStudyGruop />} /> */}
         </Route>
       </Routes>
-      {/* 채팅위젯 */}
+      {/* 채팅위젯 및 추천위젯 */}
       <ChatWidget />
+      <RecoWidget />
     </div>
   );
 }
