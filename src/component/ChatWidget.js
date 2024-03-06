98310f3a92f68e74c1062458dfa12e8e0b2c614e
yoon <whyjmlearn@gmail.com>
2024-03-06 16:24:01 +0900

    3월 6일 chatbot 수정 및 Recommend.js 만듦 추후에 수정예정

--- src/component/ChatWidget.js
+++ src/component/ChatWidget.js
@@ -1,38 +1,14 @@
 // ChatWidget.js
 import React, { useState } from "react";
-
-import { useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
 import "../styles/ChatWidget.css";
-import Chatbot from "./Chatbot.js";
+
+import CustomChatbot from "./Chatbot.js";
 
 const ChatWidget = () => {
   const [isChatVisible, setIsChatVisible] = useState(false);
-  const navigate = useNavigate();
 
   const toggleChat = () => {
-    const accessToken = localStorage.getItem("ACCESS_TOKEN");
-    const isLoggedIn = accessToken;
-
-    // 사용자가 로그인했는지 확인
-    if (isLoggedIn) {
-      setIsChatVisible((prev) => !prev);
-    } else {
-      Swal.fire({
-        icon: "info",
-        title: "로그인이 필요합니다.",
-        text: "다양한 소통을 위하여 채팅방을 이용하고 싶으신가요?",
-        showCancelButton: true,
-        confirmButtonText: "확인",
-        cancelButtonText: "취소",
-      }).then((result) => {
-        // 확인 버튼을 눌렀을 때 실행되는 함수
-        if (result.isConfirmed) {
-          navigate("/login");
-        }
-        // 취소(No) 버튼을 눌렀을 때 아무 동작 안 함
-      });
-    }
+    setIsChatVisible((prev) => !prev);
   };
 
   const closeChat = () => {
@@ -42,7 +18,7 @@ const ChatWidget = () => {
   return (
     <div className={`chat-widget ${isChatVisible ? "visible" : ""}`}>
       {isChatVisible ? (
-        <Chatbot onClose={closeChat} />
+        <CustomChatbot onClose={closeChat} />
       ) : (
         <div className="chaticon" onClick={toggleChat}>
           <img src="/assets/chat.png" alt="Chat Icon" />
