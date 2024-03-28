54e0184e3f9dfc2c9245c425cab353f3b093c8bf
yoon <whyjmlearn@gmail.com>
2024-03-28 16:39:35 +0900

    test 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import "./reset.css";
-import { getCookie } from './utils/cookie.js';
+import { getCookie } from "./utils/cookie.js";
 
 import Login from "./component/Login.js";
 import Signup from "./component/Signup.js";
 import LayOut from "./component/LayOut.js";
 import ChatWidget from "./component/chatbot/ChatWidget.js";
-import CreateStudyGruop from "./component/study/CreateStudyGroup.js";
+import CreateStudyGruop from "./component/study/createStudyGroup.js";
 import Home from "./component/Home.js";
 import RecoWidget from "./component/reco/RecoWidget.js";
 
 function App() {
   const navigate = useNavigate();
-  useEffect(() => {
-    const token = getCookie('token');
-    if (!token && window.location.pathname !== '/SignUp') {
-      navigate('/login', { replace: true });
-    }
-  }, [navigate]);
+  // useEffect(() => {
+  //   const token = getCookie("token");
+  //   if (!token && window.location.pathname !== "/SignUp") {
+  //     navigate("/login", { replace: true });
+  //   }
+  // }, [navigate]);
 
   return (
     <div className="App" id="App">
@@ -27,7 +27,11 @@ function App() {
         <Route path="/SignUp" element={<Signup />} />
         <Route path="" element={<LayOut />}>
           <Route index element={<Home />} />
-          <Route key="CreateStudyGruop" path="CreateStudyGruop" element={<CreateStudyGruop />} />
+          <Route
+            key="CreateStudyGruop"
+            path="CreateStudyGruop"
+            element={<CreateStudyGruop />}
+          />
         </Route>
       </Routes>
 
